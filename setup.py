5737f10fc60a8d0640fa3e440a9e2af740b8fc9a
h2magic <h2magic@126.com>
2023-03-22 11:21:33 +0800

    set up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="h2_common",
+    version="0.0.1",
+    description="自定义公共代码库",
+    author="h2magic",
+    author_email="h2magic@126.com",
+    url="https://github.com/h2magic-axious/PyCommon",
+    packages=find_packages(),
+    install_requires=[]
+)
